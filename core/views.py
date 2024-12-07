1b8fb1f679185df150f11c296d4a059208b891e2
ErisvaldoBalbino <erisvaldoapps@gmail.com>
2024-12-06 23:37:57 -0300

    index context + details

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from core.models import Produto
 
 # Create your views here.
+
+def index(request):
+    produtos = Produto.objects.all()
+    context = {'produtos': produtos}
+    return render(request, "core/core-index.html", context)
+
+def details(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
+    context = {'produto': produto}
+    return render(request, "core/core-details.html", context)
