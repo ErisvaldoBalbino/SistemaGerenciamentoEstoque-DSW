0dee635fc67c74312cfd231e819ce69cd5898c66
ErisvaldoBalbino <erisvaldoapps@gmail.com>
2025-01-23 17:12:51 -0300

    adicao de formularios

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from core import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('cadastrar_produto', views.cadastrar_produto, name='cadastrar_produto'),
+    path('cadastrar_fornecedor', views.cadastrar_fornecedor, name='cadastrar_fornecedor'),
+    path('cadastrar_categoria', views.cadastrar_categoria, name='cadastrar_categoria'),
     path('<int:produto_id>', views.details, name='details'),
     path('fornecedores', views.fornecedores, name='fornecedores'),
     path('categorias', views.categorias, name='categorias'),
-    path('categorias-produtos/<str:categoria_nome>', views.produtosCategorias, name='categorias-produtos'),
+    path('categorias/<str:categoria_nome>', views.produtosCategorias, name='categorias_produtos'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from core.models import Produto, Fornecedor, Categoria
-from django.views.generic.list import ListView
+from core.forms import ProdutoForm, FornecedorForm, CategoriaForm
 
 # Create your views here.
 
@@ -9,8 +9,13 @@ def index(request):
     return render(request, 'core/index.html', {'produtos': produtos})
 
 def details(request, produto_id):
-    produto = Produto.objects.get(id=produto_id)
-    return render(request, 'core/details.html', {'produto': produto})
+    produto = get_object_or_404(Produto, id=produto_id)
+    categorias = produto.categorias.all()
+    context = {
+        'produto': produto,
+        'categorias': categorias
+    }
+    return render(request, 'core/details.html', context)
 
 def fornecedores(request):
     fornecedores = get_list_or_404(Fornecedor)
@@ -21,7 +26,37 @@ def categorias(request):
     return render(request, 'core/categorias.html', {'categorias': categorias})
 
 def produtosCategorias(request, categoria_nome):
-    categoria = Categoria.objects.get(nome=categoria_nome)
+    categoria = get_object_or_404(Categoria, nome=categoria_nome)
     produtos = Produto.objects.filter(categorias=categoria)
     context = {'produtos': produtos, 'categoria': categoria}
-    return render(request, 'core/categorias-produtos.html', context)
+    return render(request, 'core/categorias_produtos.html', context)
+
+def cadastrar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ProdutoForm()
+    return render(request, 'core/cadastrar_produto.html', {'form': form})
+
+def cadastrar_fornecedor(request):
+    if request.method == 'POST':
+        form = FornecedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('fornecedores')
+    else:
+        form = FornecedorForm()
+    return render(request, 'core/cadastrar_fornecedor.html', {'form': form})
+
+def cadastrar_categoria(request):
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categorias')
+    else:
+        form = CategoriaForm()
+    return render(request, 'core/cadastrar_categoria.html', {'form': form})
